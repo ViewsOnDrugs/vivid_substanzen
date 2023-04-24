048d2a084d3d0d0bde865a53b10a3ffe5dc40300
franasa <franarsal@gmail.com>
2023-04-24 17:33:42 +0200

    add PayPal Link

--- spenden.py
+++ spenden.py
@@ -10,15 +10,16 @@ def main():
     col1, col2 = st.columns(2)
 
     with col1:
+        st.write(" ")
         if st.button('Bank Überweisung'):
             st.write("Name: VIVID e.V.")
+            st.write("Bank für Sozialwirtschaft")
             st.write("Iban: DE68 2512 0510 0001 7392 00")
-            st.write("BIC: BFSWDE33HAN")
-
-
+            st.write("BIC: BFSWDE XXX")
     with col2:
-        if st.button("PayPal"):
-            st.write("Demnächst...")
+        foot = f' [<img src="https://viatesting.files.wordpress.com/2020/03/paypal-donate-button.png" alt="drawing" width="180"/>](https://www.paypal.com/donate/?hosted_button_id=B22VZNE4Y9P68)'
+        st.markdown(foot, unsafe_allow_html=True)
+
 
     _,footcol, _ = st.columns(3)
     with footcol:
