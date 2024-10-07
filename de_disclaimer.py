d24a1bc49054e9e95bd79b3f97020984574efedb
El Pacho <75735030+franasal@users.noreply.github.com>
2024-10-07 21:41:11 +0200

    remove "-" from disclaimer de_disclaimer.py

--- de_disclaimer.py
+++ de_disclaimer.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def main():
 
-    st.write("Die Informationen auf dieser Seite sind keine Konsumempfehlung. Sie wurden sorgfältig zusammengestellt, können aber nur einen ersten Anhaltspunkt fu‌r eigene Nachforschungen bilden. Konsumerfahrungen und Risiken sind individuell. Sie können von den genannten Informationen abweichen. Sich verstärkende Kombinationen und Mischkonsum generell haben ein höheres Risiko fu‌r unerwu‌nschte Nebenwirkungen als der Monokonsum von Substanzen. Eine Verringerung der wahrgenommenen Effekte bei der Kombination von Substanzen ist meistens durch eine Überlagerung und nicht durch eine gegenseitige Aufhebung zu erklären. Die unter gefährlich genannten Kombi-nationen sind keine abschließende Liste gesundheitsschädlicher Kombinationen.")
+    st.write("Die Informationen auf dieser Seite sind keine Konsumempfehlung. Sie wurden sorgfältig zusammengestellt, können aber nur einen ersten Anhaltspunkt fu‌r eigene Nachforschungen bilden. Konsumerfahrungen und Risiken sind individuell. Sie können von den genannten Informationen abweichen. Sich verstärkende Kombinationen und Mischkonsum generell haben ein höheres Risiko fu‌r unerwu‌nschte Nebenwirkungen als der Monokonsum von Substanzen. Eine Verringerung der wahrgenommenen Effekte bei der Kombination von Substanzen ist meistens durch eine Überlagerung und nicht durch eine gegenseitige Aufhebung zu erklären. Die unter gefährlich genannten Kombinationen sind keine abschließende Liste gesundheitsschädlicher Kombinationen.")
 
 
     _,footcol, _ = st.columns(3)
